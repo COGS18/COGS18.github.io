53598cb131be9d12dd93090486bc2dfb3a6a0842
Luke <ReventonC@users.noreply.github.com>
2018-10-26 12:09:46 -0700

    cleared old site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,46 @@
+var store = [{
+        "title": "Introduction to Python",
+        "excerpt":"Thomas Donoghue Logistics Who: Instructor: Tom Donoghue TAs: Daril Brown, Paolo Gabriel, Rob Loughnan IAs: Peilin, Luis, Brandon &amp; Luke Where: Lectures MWF @ 9 am &amp; Coding Lab Sections Course Website: https://cogs18.github.io Piazza Page: https://piazza.com/ucsd/fall2018/cogs18 Expectations &amp; Approach Goal: to learn practical programming in Python How: hands-on, community driven,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/00-Introduction",
+        "teaser":null},{
+        "title": "Tools",
+        "excerpt":"This notebook will guide through the tools you will need for class materials and assignments, and how to get them. Prerequisites This course and associated materials do not presume any prior knowledge of Python, or programming in general. To work with the course materials, you will need make sure the...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/01-Tools",
+        "teaser":null},{
+        "title": "Jupyter Notebooks",
+        "excerpt":"This is a quick introduction to Jupyter notebooks. Jupyter notebooks are a way to combine executable code, code outputs, and text into one connected file.The official documentation from project Jupyter is available hereand they also have some example notebooks here.Menu Options &amp; Shortcuts To get a quick tour of the...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/02-JupyterNotebooks",
+        "teaser":null},{
+        "title": "Variables",
+        "excerpt":"Clicker #0 Please answer this iClicker question. a) okay, sure b) yes, of course! c) I don’t want to d) why are we doing thisProgramming With Python Programming: a way to ask computer to store values (variables), and do things with them (operations). # This is a comment. You can...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/03-Variables",
+        "teaser":null},{
+        "title": "Operators",
+        "excerpt":"Assignment Operator Python uses `=` for assignment.my_var = 1Math Operators Python uses mathematical operators `+`, `-`, `*`, `/` for 'sum', 'substract', 'multiply', and 'divide'. These operators return numbers.print(2 + 3)div_result = 4 / 2print(div_result)More Math Python also has `**` for exponentiation and `%` for remainder (called modulus). These also return...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/04-Operators",
+        "teaser":null},{
+        "title": "Conditionals",
+        "excerpt":"Recap: Booleans Booleans are a data type that can take values of True or False. Conditionals: if Conditionals are statements that check for a condition, using the `if` statement, and then only execute a set of code if the condition evaluates as `True`.condition = Trueif condition: print('This code executes if...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/05-Conditionals",
+        "teaser":null},{
+        "title": "Data Types",
+        "excerpt":"Collections: Lists A list a **mutable** collection of ordered items, that can be of mixed type - created using square brackets.List examples # Define a listlst = [1, 'a', True]# Print out the contents of a listprint(lst)# Check the type of a listtype(lst)Indexing Indexing refers to selecting an item from...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/06-DataTypes",
+        "teaser":null},{
+        "title": "Control Flow - Loops",
+        "excerpt":"Loops A **loop** is a procedure to repeat a piece of code.While Loops A ** while loop** is a procedure to repeat a piece of code while some condition is still met. While Loops While loops always have the structure while condition: # Loop contentsWhile condition is true, execute the...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/07-Loops",
+        "teaser":null},{
+        "title": "Encodings",
+        "excerpt":"Clicker Question #1 What is a symbol? A) A character that can be typed on a computer B) Something that can stand in the place of something else C) Something that looks like something else D) A character or sign used to represent something arbitrary E) I have no ideaSymbols...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chapters/08-Encodings",
+        "teaser":null},]
